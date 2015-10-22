9444b82d99388eb7eeb536f844220951db76b12f
Cosmic Web Services <andrewrodebaugh@cosmicsearch.org>
2015-10-22 11:25:29 -0400

    Create link.js

---
+++ link.js
@@ -0,0 +1,14 @@
+var linkjs = {
+    Version: 1.0,
+    Author: "Cosmic Web Services",
+    do : function (inputText) {
+        var replacePattern1 = /(\b(https?|ftp):\/\/[-A-Z0-9+&@#\/%?=~_|!:,.;]*[-A-Z0-9+&@#\/%=~_|])/gim;
+        var replacedText = inputText.replace(replacePattern1, '<a href="$1" target="_blank">$1</a>');
+        var replacePattern2 = /(^|[^\/])(www\.[\S]+(\b|$))/gim;
+        var replacedText = replacedText.replace(replacePattern2, '$1<a href="http://$2" target="_blank">$2</a>');
+        var replacePattern3 = /(\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,6})/gim;
+        var replacedText = replacedText.replace(replacePattern3, '<a href="mailto:$1">$1</a>');
+
+        return replacedText
+    }
+};
